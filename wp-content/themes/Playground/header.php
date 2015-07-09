b8cb29d63c8dd9f39fc65bbf3ca55d76f692afe2
VitaliySvyrydov <lobz1k3@mail.ru>
2015-07-09 23:17:29 +0300

    4-th commit

--- wp-content/themes/Playground/header.php
+++ wp-content/themes/Playground/header.php
@@ -5,19 +5,18 @@
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
-        <div id="wrapper">
-
-            <header id="masthead" class="site-header" role="banner">
-                <div class="site-branding">
-                    <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+         <header>
+                <div class="logo">
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri() ?>/images/logo.png" alt="logo">
+                    </a>
                 </div>
-            </header>
-            <?php
+                 <?php
             $args = array(
                 'theme_location' => 'primary',
                 'menu' => '',
                 'container' => 'nav',
-                'container_class' => '',
+                'container_class' => 'centeredmenu',
                 'container_id' => '',
                 'menu_class' => 'main-menu',
                 'menu_id' => 'main-menu',
@@ -29,11 +28,18 @@
                 'link_after' => '',
                 'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
                 'depth' => 0,
-                'walker' => new CustomWalkerNavMenu
             );
             if (has_nav_menu('primary')) {
                 wp_nav_menu($args);
             }
             ?>
+            </header>
+        <div id="wrapper">
+
+            <header id="masthead" class="site-header" role="banner">
+                <div class="site-branding">
+                </div>
+            </header>
+           
 
             <div id="content" class="site-content">
